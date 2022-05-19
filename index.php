652c3534b5ccc326528d07665cd9a8f53c7d82a8
MyUsername <romamargine@gmail.com>
2022-05-19 14:52:13 +0200

    Print the paragraph and its length on the screen

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.';
+var_dump($text)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+
+<body>
+    <p><?php echo $text; ?></p>
+</body>
+
+</html>
